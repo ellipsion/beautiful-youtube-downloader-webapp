b52f646d46a514db8eeb239f1120f3dde9427c00
OMEN 15 <ashishskyblue@gmail.com>
2022-05-31 23:51:16 +0530

    tweaks

--- youtube_downloader/settings.py
+++ youtube_downloader/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 WHITENOISE_USE_FINDERS = True
-
+DISABLE_COLLECTSTATIC = True
 STATIC_URL = 'static/'
 # STATICFILES_DIRS = (
 #     os.path.join(BASE_DIR, 'staticfiles'),
